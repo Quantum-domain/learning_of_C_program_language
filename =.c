64aa68fa3d289d264255c0504117fcebd8bfe4d8
Quantum <3507475670@qq.com>
2024-02-10 00:51:42 +0800

    字符串scanf测试程序。-未完善

---
+++ =.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a=0;
+    char b[4],d[4];
+    int c=0;
+    scanf("a=%d,d=%s,c=%d,b=%s",&a,&d,&c,&b);
+    printf("%d %s %d %s\n",a,d,c,b);
+    return 0;
+}
